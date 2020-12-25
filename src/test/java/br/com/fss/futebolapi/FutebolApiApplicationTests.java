d52bafed4c612c186fb8a0a5bfa365bdd3cec985
fellipe silva <fellipe.silva70@hotmail.com>
2020-12-25 01:27:19 -0300

    subindo peimeira versao

---
+++ src/test/java/br/com/fss/futebolapi/FutebolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fss.futebolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutebolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
